d8de94972c37712477ba9409fe0bd435744a3ed5
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-07-22 16:02:28 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -4,5 +4,5 @@ a=torch.randn(5, 3)
 print('original a:', a)
 indices = torch.tensor([0,0,1,1,0])
 changed_places = torch.nonzero(indices)
-a[changed_places, 0] = 1 - a[changed_places, 0]
+a[changed_places, 0] = 1 #- a[changed_places, 0]
 print('new a:', a)
