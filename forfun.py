558742685e689294d64797708ce98a537862ad17
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-07-22 16:07:23 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -3,7 +3,7 @@ import torch
 a=torch.randn(5, 3)
 print('original a:', a)
 indices = torch.tensor([0,0,1,1,0])
-changed_places = torch.nonzero(indices, as_tuple=True)
+changed_places = torch.nonzero(indices, as_tuple=False)
 print('changed_places:', changed_places)
 a[changed_places, 0] = 1 #- a[changed_places, 0]
 print('new a:', a)
