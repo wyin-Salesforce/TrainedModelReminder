b84fd448cd377d0e24f870682ca66b2abb49e789
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-07-27 11:22:01 -0700

    add GLUE

--- train_on_GLUE.py
+++ train_on_GLUE.py
@@ -127,13 +127,14 @@ def main():
         cache_dir=model_args.cache_dir,
     )
 
-    model_roberta = RobertaForSequenceClassification.from_pretrained(model_args.model_name_or_path, num_labels=3)
-    model_args.model_name_or_path = '/export/home/Dataset/BERT_pretrained_mine/TrainedModelReminder/Store_RoBERTa_From_3way_RoBERTa'
-    store_transformers_models(model_roberta.roberta, tokenizer, '/export/home/Dataset/BERT_pretrained_mine/TrainedModelReminder/', 'Store_RoBERTa_From_3way_RoBERTa')
-    print('Store_RoBERTa_From_3way_RoBERTa over...', model_args.model_name_or_path)
+    # model_roberta = RobertaForSequenceClassification.from_pretrained(model_args.model_name_or_path, num_labels=3)
+    # model_args.model_name_or_path = '/export/home/Dataset/BERT_pretrained_mine/TrainedModelReminder/Store_RoBERTa_From_3way_RoBERTa'
+    # store_transformers_models(model_roberta.roberta, tokenizer, '/export/home/Dataset/BERT_pretrained_mine/TrainedModelReminder/', 'Store_RoBERTa_From_3way_RoBERTa')
+    # print('Store_RoBERTa_From_3way_RoBERTa over...', model_args.model_name_or_path)
+    config.num_classes = 2
     model = AutoModelForSequenceClassification.from_pretrained(
         model_args.model_name_or_path,
-        from_tf=True, #bool(".ckpt" in model_args.model_name_or_path),
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
         config=config,
         cache_dir=model_args.cache_dir,
     )
