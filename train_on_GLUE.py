a461cf65aab0fce1445ce7a92d5e691832bbb496
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-07-27 12:34:59 -0700

    coding

--- train_on_GLUE.py
+++ train_on_GLUE.py
@@ -127,15 +127,15 @@ def main():
         cache_dir=model_args.cache_dir,
     )
 
-    # model_roberta = RobertaForSequenceClassification.from_pretrained(model_args.model_name_or_path, num_labels=3)
+    model_roberta = RobertaForSequenceClassification.from_pretrained(model_args.model_name_or_path, num_labels=3)
     # model_args.model_name_or_path = '/export/home/Dataset/BERT_pretrained_mine/TrainedModelReminder/Store_RoBERTa_From_3way_RoBERTa'
     # store_transformers_models(model_roberta.roberta, tokenizer, '/export/home/Dataset/BERT_pretrained_mine/TrainedModelReminder/', 'Store_RoBERTa_From_3way_RoBERTa')
     # print('Store_RoBERTa_From_3way_RoBERTa over...', model_args.model_name_or_path)
 
     model_args.model_name_or_path = 'roberta-large'
     model = AutoModelForSequenceClassification.from_pretrained(
-        'roberta-large',
-        from_tf=bool(".ckpt" in 'roberta-large'),
+        model_args.model_name_or_path,
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
         config=config,
         cache_dir=model_args.cache_dir,
     )
