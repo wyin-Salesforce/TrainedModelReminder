0c257c306df1ec4e437842fc84c0488137cf3f9e
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-07-27 14:11:30 -0700

    coding

--- train_on_GLUE.py
+++ train_on_GLUE.py
@@ -140,14 +140,14 @@ def main():
         cache_dir=model_args.cache_dir,
     )
     for name, param in model.named_parameters():
-        if param.requires_grad:
-            print(name, param.data)
+        if param.requires_grad and name == 'roberta.encoder.layer.16.attention.self.value.weight':
+            print('old:', name, param.data)
     '''it shows that .from_pretrained is different with load_state_dict(model_roberta.roberta.state_dict())'''
     model.roberta.from_pretrained('/export/home/Dataset/BERT_pretrained_mine/TrainedModelReminder/RoBERTa_on_MNLI_SNLI_SciTail_RTE_ANLI_epoch_0_acc_3.928696072567108')#load_state_dict(model_roberta.roberta.state_dict())
 
     for name, param in model.named_parameters():
-        if param.requires_grad:
-            print(name, param.data)
+        if param.requires_grad and name == 'roberta.encoder.layer.16.attention.self.value.weight':
+            print('new:', name, param.data)
 
     # Get datasets
     train_dataset = (
