3a80c3bf85fe18a98b772b0cc3c9c413656fbe8f
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-07-29 12:09:26 -0700

    coding

--- train_on_all_entail_datasets.py
+++ train_on_all_entail_datasets.py
@@ -712,13 +712,13 @@ def main():
                 # if n_gpu > 1 and not isinstance(model, torch.nn.DataParallel):
                 #     model = torch.nn.DataParallel(model)
                 model.eval()
-                # for child in model.children():
-                #     for ii in range(len(child)):
-                #         if type(child[ii])==torch.nn.BatchNorm2d:
-                #             child[ii].track_running_stats = False
-                for m in model.modules():
-                    if isinstance(m, torch.nn.BatchNorm2d):
-                        m.track_running_stats=False
+                for child in model.children():
+                    for ii in range(len(child)):
+                        if type(child[ii])==torch.nn.BatchNorm2d:
+                            child[ii].track_running_stats = False
+                # for m in model.modules():
+                #     if isinstance(m, torch.nn.BatchNorm2d):
+                #         m.track_running_stats=False
                 # logger.info("***** Running evaluation *****")
                 # logger.info("  Num examples = %d", len(valid_examples_MNLI))
                 # logger.info("  Batch size = %d", args.eval_batch_size)
