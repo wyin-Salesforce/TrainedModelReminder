1d6b9a247d9794e9c70eafa1dfed8759f1796c71
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-05 12:52:08 -0700

    coding

--- train_MRPC.py
+++ train_MRPC.py
@@ -77,12 +77,11 @@ class InputExample(object):
 class InputFeatures(object):
     """A single set of features of data."""
 
-    def __init__(self, input_ids, input_mask, segment_ids, label_id, task_label):
+    def __init__(self, input_ids, input_mask, segment_ids, label_id):
         self.input_ids = input_ids
         self.input_mask = input_mask
         self.segment_ids = segment_ids
         self.label_id = label_id
-        self.task_label = task_label
 
 
 class DataProcessor(object):
