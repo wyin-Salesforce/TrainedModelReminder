9ab393cb9246f4076252116eddbe9aae1b3bde60
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-12 12:52:24 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -1,9 +1,8 @@
 import torch
 
-a=torch.randn(5, 3)
-print('original a:', a)
-indices = torch.tensor([0,0,1,1,0])
-changed_places = torch.nonzero(indices, as_tuple=False)
-print('changed_places:', changed_places)
-a[changed_places, 0] = 1 - a[changed_places, 0]
-print('new a:', a)
+m = torch.nn.Sigmoid()
+loss = torch.nn.BCELoss()
+input = torch.randn(3, requires_grad=True)
+target = torch.empty(3).random_(2)
+output = loss(m(input), target)
+print(output)
