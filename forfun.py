be0dca5b6dd4f1655a163c08963dc79111ad323d
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-12 13:00:48 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -1,8 +1,8 @@
 import torch
 
 m = torch.nn.Sigmoid()
-loss = torch.nn.BCELoss()
-input = torch.randn(3, requires_grad=True)
-target = torch.empty(3).random_(2)
-output = loss(input, target)
+# loss = torch.nn.BCELoss()
+# input = torch.randn(3, requires_grad=True)
+# target = torch.empty(3).random_(2)
+# output = loss(input, target)
 print(m(0.0))
